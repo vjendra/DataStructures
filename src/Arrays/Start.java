5ccfab2130342bf4adbfc09ea8f6fea0d5824bbe
vshah <vshah@voceracommunications.com>
2019-06-10 13:52:06 +0530

    Stack

--- src/Arrays/Start.java
+++ src/Arrays/Start.java
@@ -1,15 +1,26 @@
 package Arrays;
 
-import java.net.SocketOption;
-
 public class Start {
 
   public static void main(String[] args) {
+
     int[] a = {0, 3, 4, 31, 35, 50, 57, 58};
     int[] b = {4, 6, 30,35, 50, 57};
     int[] c = ArrayMethods.MergeSortedArrays(a, b);
     for (int i = 0; i < c.length; i++) {
-      System.out.print(c[i]+" ");
+      System.out.println(c[i]+" ");
     }
+
+    Stack s = new Stack(5);
+    s.Push(1);
+    s.Pop();
+    s.Pop();
+    s.Push(2);
+    s.Push(3);
+    s.Push(5);
+    s.Push(5);
+    s.Push(5);
+    s.Push(6);
+    s.Pop();
   }
 }
